afa9676af9b536585b505c83eeaab1a92e045a22
Kevin Ramirez <37847309+MexpressoBean@users.noreply.github.com>
2020-02-04 23:37:04 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,89 @@
+// Variable for the number of buttons available that will play a sound
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+// Used for playing sound on a button click
+function handleClick() {
+
+  var buttonInnerHTML = this.innerHTML;
+
+  makeSound(buttonInnerHTML);
+
+  buttonAnimation(buttonInnerHTML);
+
+}
+
+// Function that plays the sound based on what button or key is chosen
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+
+      break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+  }
+}
+
+
+function buttonAnimation(currentKey)
+{
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function()
+{
+  activeButton.classList.remove("pressed");
+}, 100);
+}
+
+// Detects keyboard press
+document.addEventListener("keydown", function(event)
+{
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+});
+
+// Detects button press (with mouse click)
+for (var index = 0; index < numberOfDrumButtons; index++) {
+  document.querySelectorAll(".drum")[index].addEventListener("click", handleClick);
+}
